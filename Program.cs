1dc759bb6e34b89e0a9ee730892c5bb318566be2
Sriharsha Pallekonda <srpallek@microsoft.com>
2022-09-23 15:08:35 +0530

    minor change

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ExtractJWTsFromFile
     {
         static void Main(string[] args)
         {
-            var allLines = File.ReadAllLines(@"D:\filenameWithJWTs.txt");
+            var allLines = File.ReadAllLines(@"D:\fileContainingJWTs.txt");
             var jwtRegex = "Bearer[\\s]+([A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.)";
             long lineNo = 0;
             foreach (var line in allLines)
